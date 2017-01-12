85cc1eccc6595e168274784da865670ba6198bb2
adamb <adamdd55@gmail.com>
2017-01-12 22:34:31 +0000

    Step 2: do promotions on basket

--- src/main/scala/com/example/TddShop.scala
+++ src/main/scala/com/example/TddShop.scala
@@ -10,12 +10,38 @@ class Item(val name: String) extends AnyVal
 
 class Cart {
 
+  type promo = List[Item] => List[Item]
   val prices = Map(
     new Item("Orange") -> 25,
     new Item("Apple") -> 60
   )
-  def totalCost(items: List[Item]): Int = {
-    items.flatMap(prices.get).sum
+
+  def totalCost(items: List[Item], promos: List[promo] = List.empty): Int = {
+    val sorted = items.sortBy(_.name)
+    val afterPromotions = applyPromos(items, promos)
+    afterPromotions.flatMap(prices.get).sum
   }
-  def priceInPoundsPences(priceInPences: Int):String = f"${priceInPences.toFloat / 100}%1.2f"
+
+  def applyPromos(items: List[Item], promos: List[promo]): List[Item] = promos match {
+    case h :: t => applyPromos(h(items), t)
+    case Nil => items
+  }
+
+  def priceInPoundsPences(priceInPences: Int): String = f"${priceInPences.toFloat / 100}%1.2f"
+}
+
+object Promotions {
+  def b1g1fApple(l1: List[Item]): List[Item] = {
+    val item = new Item("Apple")
+    val my = l1.filter(_.name == item.name)
+    val other = l1.filterNot(_ == item)
+    my.take((my.size - 1) / 2 + 1) ++ other
+  }
+
+  def b3f2Orange(l1: List[Item]): List[Item] = {
+    val item = new Item("Orange")
+    val my = l1.filter(_.name == item.name)
+    val other = l1.filterNot(_ == item)
+    my.take((my.size - 2) / 3 * 2 + 2) ++ other
+  }
 }
--- src/test/scala/TddShopSpec.scala
+++ src/test/scala/TddShopSpec.scala
@@ -1,4 +1,4 @@
-import com.example.{Cart, Item}
+import com.example.{Cart, Item, Promotions}
 import org.scalatest._
 
 class TddShopSpec extends FlatSpec with Matchers {
@@ -8,6 +8,8 @@ class TddShopSpec extends FlatSpec with Matchers {
     val ap = new Item("Apple")
     val items = List(or, or, ap)
     val shop = new Cart
+    val items2 = List(or, or, ap, ap, ap)
+    val items3 = List(or, or, ap, ap, or, ap)
   }
 
   "TddShop" should "compute price" in new Context {
@@ -20,4 +22,23 @@ class TddShopSpec extends FlatSpec with Matchers {
     shop.priceInPoundsPences(1230) should be("12.30")
     shop.priceInPoundsPences(12) should be("0.12")
   }
+
+  "TddShop" should "do promo b1g1f on apples" in new Context {
+    shop.totalCost(items, List(Promotions.b1g1fApple)) should be(25 + 25 + 60)
+    shop.totalCost(items2, List(Promotions.b1g1fApple)) should be(2 * 25 + 2 * 60)
+  }
+
+  "TddShop" should "do promo 3f2 on oranges" in new Context {
+    shop.totalCost(items, List(Promotions.b3f2Orange)) should be(2 * 25 + 60)
+    shop.totalCost(items2, List(Promotions.b3f2Orange)) should be(2 * 25 + 3 * 60)
+    shop.totalCost(items3, List(Promotions.b3f2Orange)) should be(2 * 25 + 3 * 60)
+    shop.totalCost(items3 ++ items3 ++ items2, List(Promotions.b3f2Orange)) should be((2 * 2 + 2) * 25 + (3 + 2 * 3) * 60)
+  }
+
+  "TddShop" should "do promo together" in new Context {
+    shop.totalCost(items, List(Promotions.b3f2Orange, Promotions.b1g1fApple)) should be(2 * 25 + 60)
+    shop.totalCost(items, List(Promotions.b1g1fApple, Promotions.b3f2Orange)) should be(2 * 25 + 60)
+    shop.totalCost(items3 ++ items3 ++ items3, List(Promotions.b3f2Orange, Promotions.b1g1fApple)) should be((3 * 2) * 25  + (4 + 1) * 60)
+  }
+
 }
